ce4a0c1a0ee9164971533a99eb3aa01a1862f0fa
gofoward-z <zlepeng.ucf@gmail.com>
2022-06-21 17:12:42 -0400

    add ability push data to server

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+//parse incoming string or array data
+app.use(express.urlencoded({extended: true}));
+//parse incoming JSON data
+app.use(express.json());
 
 const { animals } = require('./data/animals.json');
 
@@ -51,6 +59,20 @@ function findById(id, animalsArray) {
     return result;
 }
 
+function createNewAnimal(body,animalsArray) {
+    const animal = body;
+    //our function's main code will go here!
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+
+
+    //return finished code to post route for response
+    return body;
+}
+
 
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -69,6 +91,39 @@ app.get('/api/animals/:id', (req, res) => {
     }
     
 });
+
+app.post('/api/animals', (req,res) => {
+    //req.body is where our incoming content will be
+    req.body.id = animals.length.toString();
+
+    //if any data in req.boday is incorrect,send 400 error back
+    if(!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else{
+        //add animal to jason file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+
+    }
+
+    
+});
+
+function validateAnimal(animal) {
+    if(!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if(!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if(!animal.diet || typeof animal.species !== 'string') {
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+}
 
 
 app.listen(PORT, () => {
